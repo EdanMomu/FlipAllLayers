c156ede164a19871b28d4c31527df827bc61b4c3
Edan Moz <blagatass@gmail.com>
2021-01-01 19:23:45 -0300

    Fixed error when working with invisible layers. Now will flip invisible layers and keep them invisible.

--- Flip_All_Layers.jsx
+++ Flip_All_Layers.jsx
@@ -1,6 +1,7 @@
 //Script to flip all layers horizontally
 //Just collect all layers in an array and then flip each one by looping
 //if background layer is present it has to be transformed into a normal layer, then flipped, and then turned back into a
+//Not really recomended for multiple frame animations.
 
 //get the document
 doc = app.activeDocument;
@@ -11,13 +12,23 @@ allLayers = collectAllLayers(doc, allLayers);
 
 //loop and flip
 for (var i = 0; i < allLayers.length; i++) {
-    if (allLayers[i].isBackgroundLayer == false) { //flip all layers that aren't backgorund
-        allLayers[i].resize(-100,undefined);
+    //flip all layers that aren't backgorund
+    if (allLayers[i].isBackgroundLayer == false) {
+        
+        var vis = allLayers[i].visible;                                         //save whether the layer was visible or not
+
+        allLayers[i].visible = true;                                            //set to visible so the layer is editable
+        allLayers[i].resize(-100, undefined, AnchorPosition.MIDDLECENTER);      //flip the layer
+        allLayers[i].visible = vis;                                             //set the visibility back to what it was before editing
     }
+
     else {
-        allLayers[i].isBackgroundLayer = false; //transform background layer into normal layer
-        allLayers[i].resize(-100,undefined); //flip it
-        allLayers[i].isBackgroundLayer = true; //turn back into backgorund layer
+    //flip the backgorund layer (if existant)
+        allLayers[i].visible = true;                                            //set to visible so the layer is editable
+        allLayers[i].isBackgroundLayer = false;                                 //transform background layer into normal layer
+        allLayers[i].resize(-100, undefined, AnchorPosition.MIDDLECENTER);      //flip it
+        allLayers[i].isBackgroundLayer = true;                                  //turn back into backgorund layer
+        allLayers[i].visible = vis;                                             //set the visibility back to what it was before editing
     }
 }
 
